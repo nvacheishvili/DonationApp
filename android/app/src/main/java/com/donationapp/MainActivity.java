ab87a873027af051396bb710eb0fc13b03a9ba64
Nata Vacheishvili <natashkela@gmail.com>
2023-05-03 19:31:42 -0400

    installing navigation system

--- android/app/src/main/java/com/donationapp/MainActivity.java
+++ android/app/src/main/java/com/donationapp/MainActivity.java
@@ -4,6 +4,7 @@ import com.facebook.react.ReactActivity;
 import com.facebook.react.ReactActivityDelegate;
 import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
 import com.facebook.react.defaults.DefaultReactActivityDelegate;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -32,4 +33,9 @@ public class MainActivity extends ReactActivity {
         DefaultNewArchitectureEntryPoint.getConcurrentReactEnabled() // concurrentRootEnabled
         );
   }
+
+  @Override
+    protected void onCreate(Bundle savedInstanceState) {
+      super.onCreate(null);
+    }
 }
