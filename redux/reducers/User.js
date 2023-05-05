f567b29d5cafc524aa4d3d095d364cc0ceeaf358
Nata Vacheishvili <natashkela@gmail.com>
2023-05-04 20:45:45 -0400

    created our first slice

---
+++ redux/reducers/User.js
@@ -0,0 +1,27 @@
+// Importing the createSlice function from the Redux Toolkit
+import {createSlice} from '@reduxjs/toolkit';
+
+// Defining the initial state for the user slice of the store
+const initialState = {
+  firstName: 'Nata',
+  lastName: 'Vacheishvili',
+  userId: 1,
+};
+
+// Creating a new slice of the store named "user" with its own set of reducers
+export const User = createSlice({
+  name: 'user',
+  initialState: initialState,
+  reducers: {
+    // Defining the "updateFirstName" reducer function
+    // It takes the current state and an action object as parameters
+    // It updates the firstName field of the state with the payload value of the action
+    updateFirstName: (state, action) => {
+      state.firstName = action.payload.firstName;
+    },
+  },
+});
+
+// Exporting the reducers here from the "User" slice
+// makes them available to other parts of the app that want to use it
+export const {updateFirstName} = User.actions;
