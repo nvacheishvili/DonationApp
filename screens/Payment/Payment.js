ea83fe3c37f3766008913639cb80403860b0105a
Nata Vacheishvili <natashkela@gmail.com>
2023-05-10 19:23:06 -0400

    localhost server side code integration

--- screens/Payment/Payment.js
+++ screens/Payment/Payment.js
@@ -1,17 +1,60 @@
-import React from 'react';
-import {SafeAreaView, ScrollView, View, Text} from 'react-native';
+import React, {useState} from 'react';
+import {SafeAreaView, ScrollView, View, Text, Alert} from 'react-native';
 import globalStyle from '../../assets/styles/globalStyle';
 import Header from '../../components/Header/Header';
 import style from './style';
 import {useSelector} from 'react-redux';
 import Button from '../../components/Button/Button';
-import {CardForm, StripeProvider} from '@stripe/stripe-react-native';
+import {
+  CardForm,
+  StripeProvider,
+  useConfirmPayment,
+} from '@stripe/stripe-react-native';
 import {STRIPE_PUBLISHABLE_KEY} from '../../constants/App';
 
 const Payment = ({navigation}) => {
   const donationItemInformation = useSelector(
     state => state.donations.selectedDonationInformation,
   );
+
+  const [isReady, setIsReady] = useState(false);
+  const {confirmPayment, loading} = useConfirmPayment();
+  const user = useSelector(state => state.user);
+
+  //Make sure to start your local server with nodemon index.js before running this, otherwise your local server will not receive your requests
+  const fetchPaymentIntentClientSecret = async () => {
+    const response = await fetch(
+      'http://localhost:3000/create-payment-intent',
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email: user.email,
+          currency: 'usd',
+          amount: donationItemInformation.price * 100,
+        }),
+      },
+    );
+    const {clientSecret} = await response.json();
+    return clientSecret;
+  };
+  const handlePayment = async () => {
+    const clientSecret = await fetchPaymentIntentClientSecret();
+    const {error, paymentIntent} = await confirmPayment(clientSecret, {
+      paymentMethodType: 'Card',
+    });
+    if (error) {
+      Alert.alert(
+        'Error has occured with your payment',
+        error.localizedMessage,
+      );
+    } else if (paymentIntent) {
+      Alert.alert('Successful', 'The payment was confirmed successfully!');
+      navigation.goBack();
+    }
+  };
 
   return (
     <SafeAreaView style={[globalStyle.backgroundWhite, globalStyle.flex]}>
@@ -22,12 +65,21 @@ const Payment = ({navigation}) => {
         </Text>
         <View>
           <StripeProvider publishableKey={STRIPE_PUBLISHABLE_KEY}>
-            <CardForm style={style.cardForm} />
+            <CardForm
+              style={style.cardForm}
+              onFormComplete={() => {
+                setIsReady(true);
+              }}
+            />
           </StripeProvider>
         </View>
       </ScrollView>
       <View style={style.button}>
-        <Button title={'Donate'} />
+        <Button
+          title={'Donate'}
+          isDisabled={!isReady || loading}
+          onPress={async () => await handlePayment()}
+        />
       </View>
     </SafeAreaView>
   );
